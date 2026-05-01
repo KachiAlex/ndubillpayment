dbce9c0bd094d81ebbef2545f8615c40dd77791e
Cleanjohn Dev <dev@cleanjohn.com>
2026-05-01 22:59:34 +0100

    Remove serverless-http, use minimal Promise wrapper instead to debug timeout

--- api/index.js
+++ api/index.js
@@ -1,15 +1,14 @@
-console.log('[api/index] Module load started');
-
-const serverless = require('serverless-http');
-console.log('[api/index] serverless-http loaded');
-
 const app = require('../backend/app');
-console.log('[api/index] backend/app loaded');
-
-const handler = serverless(app);
-console.log('[api/index] serverless wrapper created');
 
 module.exports = (req, res) => {
-  console.log('[api/index] Request received:', req.url);
-  return handler(req, res);
+  return new Promise((resolve, reject) => {
+    res.on('finish', resolve);
+    res.on('error', reject);
+    app(req, res, (err) => {
+      if (err && !res.headersSent) {
+        res.status(500).json({ error: err.message });
+      }
+      resolve();
+    });
+  });
 };
